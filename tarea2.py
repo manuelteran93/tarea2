f45e953ebb531189f8c27f4d380b8867a1012c14
manuelteran93 <manuelteran93@gmail.com>
2022-08-05 11:35:18 -0300

    tarea2

---
+++ tarea2.py
@@ -0,0 +1,8 @@
+""" Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
+
+Tienes que subir capturas de pantalla en una carpeta comprimida zip. """
+
+
+x = ("Hola Mundo!")
+print(x) 
+
